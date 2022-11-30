f4645f58973fc7bfbf2649663fbe783e329bb400
Chris Ormsby <ormsbyx@pm.me>
2022-11-30 07:52:48 -0500

    fork of lit-element typescript starter from official lit repo
    fork of lit-element typescript starter from official lit repo

---
+++ jco-readme.txt
@@ -0,0 +1,5 @@
+% npm run serve seems to run the webserver, but not compile typescript
+
+you need to also run:
+
+# npm run build:watch to compile .ts files
--- src/my-element.ts
+++ src/my-element.ts
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {LitElement, html, css, PropertyValues } from 'lit';
+import {customElement, property } from 'lit/decorators.js';
 
 /**
  * An example element.
@@ -31,25 +31,81 @@ export class MyElement extends LitElement {
   @property()
   name = 'World';
 
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({type: Number})
-  count = 0;
+  @property({
+    type: Number,
+    attribute: 'count-start',
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    converter(value: any) { // converts the string from the attribute to whatever
+      return value ? (value % 100) * 3 : 0;
+    },
+    hasChanged(newVal: number) { // only request an update if newVal is odd
+      return (newVal % 2) == 1;
+    }
+  })
+  count = 23;
+
+  @property({ type: Object, attribute: 'extra-object' })
+  extraObject: { name: String | undefined, age: Number | undefined } = {
+    name: undefined,
+    age: undefined
+  };
+
+  @property({ reflect: true, converter: {
+    toAttribute: (value) => {
+      return `chris-string-${value}`
+    },
+    fromAttribute: (value: string) => {
+      console.log('fromAttribute:', `from-attribute-${value}`);
+      return `from-attribute-${value}`;
+    }
+  }
+  })
+  thingamaJig = "asdf";
+
+  constructor() {
+    super();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).mycomp = this;
+  }
+
+  override connectedCallback() {
+    super.connectedCallback();
+  }
 
   override render() {
+    setTimeout(async () => {
+      await this.updateComplete;
+      console.log('AFTER UPDATE COMPLETE (in render())!', this.count);
+    }, 0);
     return html`
       <h1>${this.sayHello(this.name)}!</h1>
       <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
+        Click counter! ${this.count}
       </button>
       <slot></slot>
+      <div>Name: ${this.defaultName} - age: ${this.defaultAge}</div>
     `;
   }
 
-  private _onClick() {
+  private get defaultName(): String {
+    return this.extraObject && this.extraObject.name ? this.extraObject.name : 'Phil';
+  }
+
+  private get defaultAge(): Number {
+    return this.extraObject && this.extraObject.age ? this.extraObject.age : 18;
+  }
+
+  private async _onClick() {
     this.count++;
     this.dispatchEvent(new CustomEvent('count-changed'));
+    try {
+      await this.updateComplete;
+      console.log('AFTER UPDATE COMPLETE (in _onClick())!', this.count);
+    } catch (e) {
+      console.log('AFTER UPDATE COMPLETE (in _onClick()) -- ERROR', e, this.count);
+    }
+
   }
 
   /**
@@ -59,10 +115,43 @@ export class MyElement extends LitElement {
   sayHello(name: string): string {
     return `Hello, ${name}`;
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  override shouldUpdate(_changedProperties: PropertyValues<this>): boolean {
+    console.log('should update:', _changedProperties);
+    return true;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  override async willUpdate(_changedProperties: PropertyValues<this>) {
+    // eslint-disable-next-line prefer-rest-params
+    console.log('willUpdate:', _changedProperties);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  override updated(_changedProperties: Map<string, any>) {
+    console.log('updated:', _changedProperties);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  override async update(_changedProperties: Map<string, any>) {
+    console.log('update interceptor: ', _changedProperties);
+    super.update(_changedProperties);
+  }
+
+  override async getUpdateComplete() {
+    const result = super.getUpdateComplete();
+    // do other stuff
+    return result;
+  }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
     'my-element': MyElement;
   }
+}
+
+window.onunhandledrejection = function(e) {
+  console.log('MY UNHANDLED EXCEPTION:', e);
 }
